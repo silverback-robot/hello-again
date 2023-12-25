e4ead11da34d42d1dc5f785f764b9c262695354c
Harish Sundaresh <yesharish@yahoo.in>
2023-12-25 17:45:13 +1100

    handle empty string input

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Meera")
-	want := "Hello, Meera"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("say hello to the named person", func(t *testing.T) {
+
+		got := Hello("Meera")
+		want := "Hello, Meera"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
