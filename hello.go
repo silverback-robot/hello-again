e9d1008a9d86abc5ab0f5547bfc62aff10f1344e
Harish Sundaresh <yesharish@yahoo.in>
2024-05-19 13:47:17 +1000

    say 'Hello, World! if no name supplied

--- hello.go
+++ hello.go
@@ -5,10 +5,13 @@ import "fmt"
 const englishPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World!"
+	}
 	return englishPrefix + name
 }
 
 func main() {
 
-	fmt.Println(Hello("Meera"))
+	fmt.Println(Hello(""))
 }
